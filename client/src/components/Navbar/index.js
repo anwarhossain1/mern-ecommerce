25f3c411c04e0e1cf1670af4f629c1bf3238a309
anwarhossain1 <irishkhan33@gmail.com>
2021-09-12 11:26:02 +0600

    updating login button to user email address after login

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,6 +6,8 @@ export default function Index() {
   const dispatch = useDispatch();
   const addtocartreducer = useSelector((state) => state.addToCartReducer);
   const { cartItems } = addtocartreducer;
+  const loginUser = JSON.parse(localStorage.getItem("currentUser"));
+  console.log(loginUser);
 
   return (
     <div>
@@ -30,15 +32,55 @@ export default function Index() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="/login"
-                >
-                  Login
-                </a>
-              </li>
+              {loginUser ? (
+                // <li className="nav-item text-white mt-2">
+                //   <a>{loginUser.name}</a>
+                // </li>
+                <li className="nav-item dropdown ">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="true"
+                  >
+                    {loginUser.email}
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="/profile">
+                        Profile
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/orders">
+                        Orders
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item">Logout</a>
+                    </li>
+                  </ul>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <a
+                    className="nav-link active"
+                    aria-current="page"
+                    href="/login"
+                  >
+                    Login
+                  </a>
+                </li>
+              )}
+
               <li className="nav-item">
                 <a className="nav-link" href="/cartdetails">
                   <i class="fas fa-cart-plus"></i>
